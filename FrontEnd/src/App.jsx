dfb25f5cc63d3ef0fa3d554d154b854fd13a37a0
boyoungkk33 <boyoungkk33@naver.com>
2023-05-31 19:19:29 +0900

    수정 출력x

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,19 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Outlet, Route, Routes } from 'react-router-dom'
 import './App.css'
+import LandingPage from './pages/LandingPage'
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
+import Navbar from './layout/NavBar'
+import Footer from './layout/Footer'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Layout() {
   return (
-      <div className="App">
-        <h1 className='text-3xl font-bold underline'>
-          Hello world!
-        </h1>
-        </div>
-       
-  )
+    <div>
+      <Navbar />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+function App() {
+  return (
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        {/* 로그인과 상관없이 갈 수 있는 경로 */}
+        <Route index element={<LandingPage />} />
+
+        {/* 로그인한 사람은 갈 수 없는 경로 */}
+        <Route element={<NotAuthRoutes />}>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Route>
+
+        {/* 로그인한 사람만 갈 수 있는 경로 */}
+        <Route element={<ProtectedRoutes />}>
+          <Route path="/product/upload" element={<UploadProductPage />} />
+          <Route path="/product/:productId" element={<DetailProductPage />} />
+          <Route path="/user/cart" element={<CartPage />} />
+          <Route path="/history" element={<HistoryPage />} />
+        </Route>
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
