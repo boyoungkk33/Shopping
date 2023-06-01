a5608f4f90beda919866850892bd74b40c8c92d5
boyoungkk33 <boyoungkk33@naver.com>
2023-06-01 19:17:46 +0900

    Backend 구조생성

--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -1,15 +1,45 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const cors = require('cors');
+const port = 4000;
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
+app.use(cors());
+app.use(express.json());
 
-console.log(path.join(__dirname, '/uploads'));
-app.use(express.static(path.join(__dirname, 'uploads')));
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('연결 완료');
+    })
+    .catch(err => {
+        console.error(err);
+    })
+
+app.get('/', (req, res, next) => {
+    setImmediate(() => { next(new Error('it is an error')) });
+    // res.send('안녕하세요.2222');
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.json(req.body);
+})
+
+
+//app.use('/users', require('./routes/users'));
+
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.send(error.message || '서버에서 에러가 났습니다.');
+})
+
+
+app.use(express.static(path.join(__dirname, '../uploads')));
 
 app.listen(port, () => {
-  console.log(`${port}번에서 실행되었습니다.`);
+    console.log(`${port}번에서 실행이 되었습니다.`);
 });
