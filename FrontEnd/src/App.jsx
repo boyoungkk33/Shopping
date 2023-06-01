e9e7e9a98050a519bc2d17db5e4967497ea32b9f
boyoungkk33 <boyoungkk33@naver.com>
2023-06-01 16:18:45 +0900

    FrontEnd 구성 및 BackEnd 생성

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -25,6 +25,7 @@ function App() {
         {/* 로그인과 상관없이 갈 수 있는 경로 */}
         <Route index element={<LandingPage />} />
 
+
         {/* 로그인한 사람은 갈 수 없는 경로 */}
         <Route element={<LoginPage />}>
           <Route path="/login" element={<LoginPage />} />
--- FrontEnd/src/layout/Footer/index.jsx
+++ FrontEnd/src/layout/Footer/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {AiOutlineSmile} from "react-icons/ai";
 const Footer = () => {
+  
   return (
     <div className='flex h-20 text-lg justify-center items-center'>
      All right reserved.<AiOutlineSmile/>
